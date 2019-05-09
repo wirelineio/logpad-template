276aca1e4838a90731618aa9fab8c106f6737743
Rich Burdon <rich.burdon@gmail.com>
2019-05-09 10:13:14 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
-//
-// Copyright 2019 Wireline, Inc.
-//
-
 import EditIcon from '@material-ui/icons/Edit';
 
 import { view, type } from './def';
 import Pad from './Pad';
 
 export default {
-  title: 'My Example',
+  title: 'LogPad',
   type,
   view,
   main: Pad,
