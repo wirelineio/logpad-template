2e4540e40514adf40bcc739f35353478f661ba9a
Max Fierro <elmasse@gmail.com>
2019-08-12 18:00:12 -0300

    Update title

--- src/defs.js
+++ src/defs.js
@@ -2,7 +2,7 @@ import Icon from '@material-ui/icons/Check';
 import color from '@material-ui/core/colors/purple';
 
 export default {
-  title: 'LogPad',
+  title: 'Taskpad',
   view: 'example',
   icon: Icon,
   color: color[400]
