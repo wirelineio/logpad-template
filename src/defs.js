f95058d8d772ef238dee8e0790da84dc980478f3
richburdon <rich.burdon@gmail.com>
2019-08-13 16:44:52 +0100

    Change name

--- src/defs.js
+++ src/defs.js
@@ -6,7 +6,7 @@ import Icon from '@material-ui/icons/Check';
 import color from '@material-ui/core/colors/purple';
 
 export default {
-  title: 'Taskpad',
+  title: 'Tasks',
   view: 'example',
   icon: Icon,
   color: color[400]
